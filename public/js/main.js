c2607fe5fabca0c135711edbcb5d4293429c667d
Kik Minev <kikminev@gmail.com>
2019-11-23 17:31:20 +0200

    Change social icons

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,19 @@ uSite = {
 
         $(element).css('margin-top', marginTop+'px');
         $(element).animate({'opacity' : 1}, 1000);
+    },
+    centerElementVertically: function (element) {
+        const verticalCener = Math.floor(window.innerHeight/2);
+        const elementHeight = $(element).height();
+
+        let marginTop = verticalCener - (elementHeight / 2);
+
+        if(marginTop <= 0) {
+            marginTop = verticalCener;
+        }
+
+        $(element).css('margin-top', marginTop+'px');
+        $(element).animate({'opacity' : 1}, 1000);
     }
 };
 
