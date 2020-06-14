8d7b92097368010f5bfbb497822a6cf34c29fd6e
Kik <kikminev@gmail.com>
2020-06-14 23:02:31 +0300

    Add unique constraint to blog post

---
+++ src/Validation/Site/Slug/Post/SlugIsUniqueValidator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Validation\Site\Slug\Post;
+
+use App\Document\Post;
+use App\Repository\PostRepository;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class SlugIsUniqueValidator extends ConstraintValidator
+{
+    private PostRepository $postRepository;
+    /**
+     * @var TranslatorInterface
+     */
+    private TranslatorInterface $translator;
+
+    public function __construct(PostRepository $postRepository, TranslatorInterface $translator)
+    {
+        $this->postRepository = $postRepository;
+        $this->translator = $translator;
+    }
+
+    public function validate($value, Constraint $constraint): void
+    {
+        $existingPost = $this->postRepository->findOneBy(['slug' => $value]);
+        if(null === $existingPost) {
+            return;
+        }
+
+        /** @var Post $validatedPost */
+        $validatedPost = $this->context->getObject();
+
+        if ($validatedPost->getId() !== $existingPost->getId()) {
+            $this->context->buildViolation($this->translator->trans('form_post_already_used'))->addViolation();
+        }
+    }
+}
